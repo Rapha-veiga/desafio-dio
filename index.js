2f4894f9f69e72e5ff7a620f06027d26e87c0f16
Rapha-veiga <raphaelcontactwork@gmail.com>
2024-10-24 08:18:36 -0300

    meu quarto commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-let nome = "raphael"
-console.log("Olá mundo")
+let nomeheroi = ["Homem-Aranha", "Hulk", "Capitão América"]
+let xp = [1000, 2000, 3500, 4630, 5000, 6550, 7421, 8000, 9210, 10001]
+
+if(xp == 1000) {
+xp = "Ferro"
+}
+else if(xp < 1000 > 2001) {
+xp = "Bronze"
+}
+else if(xp < 2001 > 3001) {
+xp = "Prata"
+}
