6d2bb5f98bae4f9c8b25e15478c5ac911a16380a
Rapha-veiga <raphaelcontactwork@gmail.com>
2024-10-24 08:54:14 -0300

    variáveis

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ let nomeheroi = ["Homem-Aranha", "Hulk", "Capitão América"]
 let xp = [1000, 2000, 3500, 4630, 5000, 6550, 7421, 8000, 9210, 10001]
 
 if(xp == 1000) {
-xp = "Ferro"
+fase = "Ferro"
 }
 else if(xp < 1000 > 2001) {
-xp = "Bronze"
+fase = "Bronze"
 }
 else if(xp < 2001 > 3001) {
-xp = "Prata"
+fase = "Prata"
+}
+else if(xp < 3001 > 4001) {
+fase = "Ouro"
+}
+else if(xp < 4001 > 5001) {
+fase = "Platina"
 }
