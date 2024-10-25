8fc05cc5c54050d92bf1d1900407b40a5ae00582
Rapha-veiga <raphaelcontactwork@gmail.com>
2024-10-25 07:53:54 -0300

    Programa finalizado

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-let nomeheroi = ["Homem-Aranha", "Hulk", "Capitão América"]
-let xp = [1000, 2000, 3500, 4630, 5000, 6550, 7421, 8000, 9210, 10001]
+let nomeheroi = ["Homem-Aranha", "Hulk", "Capitão América"];
+let fase = [1000, 2000, 3500, 4630, 5000, 6550, 7421, 8000, 9210, 10001];
 
-if(xp == 1000) {
-fase = "Ferro"
+if (xp >= 1000) {
+  fase = "Ferro";
+} else if (xp < 1000 > 2001) {
+  fase = "Bronze";
+} else if (xp < 2001 > 5001) {
+  fase = "Prata";
+} else if (xp < 5001 > 7000) {
+  fase = "Ouro";
+} else if (xp < 7001 > 8000) {
+  fase = "Platina";
+} else if (xp < 8001 > 9000) {
+  fase = "Ascendente";
+} else if (xp < 9001 > 10000) {
+  fase = "Imortal";
+} else if (xp <= 10001) {
+  fase = "Radiante";
 }
-else if(xp < 1000 > 2001) {
-fase = "Bronze"
-}
-else if(xp < 2001 > 3001) {
-fase = "Prata"
-}
-else if(xp < 3001 > 4001) {
-fase = "Ouro"
-}
-else if(xp < 4001 > 5001) {
-fase = "Platina"
-}
+
+console.log ("O herói de nome " + nomeheroi[1] + "está no nível " + fase[3])
