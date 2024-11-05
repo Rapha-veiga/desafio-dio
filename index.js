0609b911b41b3d029da411d889e80dff5c1e80ec
Rapha-veiga <raphaelcontactwork@gmail.com>
2024-11-05 07:34:59 -0300

    commit teste

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
   function calcularPontos (jogador, vitorias, derrotas){
     let mediaPontos = vitorias - derrotas;
+    let nivel
 
     if (mediaPontos < 10) {
       nivel = "Ferro"
 
     }
-    if (mediaPontos == 11 && 20) {
+    if (mediaPontos == 11 <= 20) {
       nivel = "Bronze"
       
     }
-    if (mediaPontos < 10) {
+    if (mediaPontos == 21 <= 50) {
+      nivel = "Prata"
+      
+    }
+    if (mediaPontos == 51 <= 80) {
+      nivel = "Ouro"
+      
+    }
+    if (mediaPontos == 81 <= 90) {
+      nivel = "Diamante"
+      
+    }
+    if (mediaPontos == 91 <= 100) {
       nivel = "Ferro"
       
     }
-    if (mediaPontos < 10) {
-      nivel = "Ferro"
-      
-    }
-    if (mediaPontos < 10) {
-      nivel = "Ferro"
-      
-    }
-    if (mediaPontos < 10) {
-      nivel = "Ferro"
-      
-    }
-    if (mediaPontos < 10) {
-      nivel = "Ferro"
-      
-    }
-    if (mediaPontos < 10) {
-      nivel = "Ferro"
+    if (mediaPontos >= 101) {
+      nivel = "Imortal"
       
     }
 
-    console.log ("O jogador " + jogador + " tem um saldo de " + mediaPontos + " pontos e está no nível ")
+    console.log ("O jogador " + jogador + " tem um saldo de " + mediaPontos + " pontos e está no nível " + nivel)
  }
 
- calcularPontos("Matheuzinho Gameplay", 20, 15)
+ calcularPontos("Matheuzinho Gameplay", 80, 12)
